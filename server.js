90c5f6b2025c2806e4a46ca2747c81a67b453b20
Helmidh <76059758+Helmidh@users.noreply.github.com>
2021-06-26 15:39:07 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 //const sql = require("./config_DB/db");
 
@@ -7,24 +7,16 @@ const app = express();
 //app.use(bodyParser);
 
 // parse requests of content-type: application/json
-app.use(express.json());
+app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.json());
 //app.use(express.urlencoded());
 // set port, listen for requests
 require("./src/routers/voyage.router")(app);
 require("./src/routers/personnel.router")(app);
-require("./src/routers/task.router")(app);
-
-// Require employee routes
-const hotel = require('./src/routers/role.router')(app);
-// using as middleware
-//app.use('/hotels', hotel)
-
-
 app.listen(3002, () => {
     //console.log(sql);
    console.log("Server is running on port 3002.");
